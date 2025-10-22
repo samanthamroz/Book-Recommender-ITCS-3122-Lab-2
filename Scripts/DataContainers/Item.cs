6b27d326d66eb65ca155823f5deecf0b72cccb03
Samantha Mroz <smroz1@charlotte.edu>
2025-10-21 20:56:00 -0400

    fixed encapsulation violation

--- Scripts/DataContainers/Item.cs
+++ Scripts/DataContainers/Item.cs
@@ -1,7 +1,7 @@
 namespace Lab2;
 
 public abstract class Item {
-    public int ItemId;
+    public int ItemId { get; init; }
     public abstract override string ToString();
     public static string GetItemTypes() {
         return "BOOK";
--- Scripts/DataContainers/User.cs
+++ Scripts/DataContainers/User.cs
@@ -1,8 +1,8 @@
 namespace Lab2;
 
 public abstract class User {
-    public string Name;
-    public int UserId;
+    public string Name { get; init; }
+    public int UserId { get; init; }
     public abstract override string ToString();
     public static string GetUserTypes() {
         return "MEMBER";
